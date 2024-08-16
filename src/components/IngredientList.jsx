a812e62db92cdd601a8d1cf35d6d6eae918fed57
gabtongol <geybtongol@gmail.com>
2024-08-15 20:40:39 -0400

    done and changed a bit of styling

---
+++ src/components/IngredientList.jsx
@@ -0,0 +1,19 @@
+// src/components/IngredientList.jsx
+import '../App.css'
+
+const IngredientList = (props) => {
+    return(
+        <ul>
+          {props.availableIngredients.map((ingredient, index) => (
+               <li key={index} style={{ backgroundColor: ingredient.color}}>
+                    {ingredient.name}
+
+                    <button onClick={() => props.addToBurger(ingredient)}>+</button>
+               </li>
+          ))}
+        </ul>
+    );
+  };
+  
+  export default IngredientList;
+  
